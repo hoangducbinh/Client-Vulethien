6c3d0e9cffee7d908a6b828e18e5751fa0233c8e
hoangducbinh <hoangbinh02022002@gmail.com>
2024-08-28 23:52:50 +0700

    fix

---
+++ src/routers/AuthRouter.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AuthRouter = () => {
+  return (
+    <Link href="/login">
+        Nhấn vào đây để đăng nhập
+    </Link>
+  )
+}
+
+export default AuthRouter
